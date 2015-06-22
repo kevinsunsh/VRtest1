13334aad252eeed8ede70f6fb6bb384b6985505b
kevinsunsh <sunzhipeng8@gmail.com>
2015-06-22 21:42:08 +0800

    init commit

---
+++ Assets/Cardboard/DemoScene/SpartankingAnimate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpartankingAnimate : MonoBehaviour {
+	string mAnimationName;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Animation> ().PlayQueued (mAnimationName);
+		if(!GetComponent<Animation> ().IsPlaying(mAnimationName))
+			GetComponent<Animation> ().Play (mAnimationName);
+	}
+
+	public void SetGazedAt(bool gazedAt) {
+		//	System.Console.WriteLine("kevin debug test");
+		if (gazedAt)
+			mAnimationName = "run";
+		else
+			mAnimationName = "idle";
+	}
+}
